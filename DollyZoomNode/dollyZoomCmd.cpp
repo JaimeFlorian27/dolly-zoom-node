e96d77f0ac4cfd731ec317bb226c68278bef0556
DESKTOP-9OEBHRC\jflor <jaimeflorian27@gmail.com>
2022-09-02 20:03:59 -0700

    implemented barebones doIt function, testing if syntax implementation is correct

--- DollyZoomNode/dollyZoomCmd.cpp
+++ DollyZoomNode/dollyZoomCmd.cpp
@@ -1,4 +1,6 @@
 #include "dollyZoomCmd.h"
+
+MSelectionList DollyZoomCmd::selectionList;
 DollyZoomCmd::DollyZoomCmd()
 {
 }
@@ -11,14 +13,53 @@ void* DollyZoomCmd::creator()
 }
 
 MSyntax DollyZoomCmd::newSyntax()
-{
+{	//Define syntax for command (2 arguments needed, camera name and target name)
 	MSyntax syntax;
 	syntax.addArg(MSyntax::kString);
 	syntax.addArg(MSyntax::kString);
-	syntax.setObjectType(MSyntax::kStringObjects, 2, 2);
+	//return the objects as a selection list
+	syntax.setObjectType(MSyntax::kSelectionList, 2, 2);
 	return syntax;
 }
 MStatus DollyZoomCmd::doIt(const MArgList& args)
 {
-	return MStatus();
+	MStatus status;
+	//use the database to store the objects taken in using our syntax into my internal class.
+	MArgDatabase argData(newSyntax(), args);
+	status = argData.getObjects(selectionList);
+	CHECK_MSTATUS_AND_RETURN_IT(status);
+	//call to redoIt for execution of command
+	redoIt();
+	return status;
+}
+
+MStatus DollyZoomCmd::redoIt()
+{
+	MStatus status;
+
+	//check if there are less than two arguments
+	if (selectionList.length() < 2) {
+		status = MS::kFailure;
+		CHECK_MSTATUS_AND_RETURN_IT(status);
+	}
+
+	
+
+
+
+	//create node
+
+	MFnDependencyNode dollyFn;
+	MObject dollyObj;
+
+	dollyObj = dollyFn.create("dollyZoom");
+
+	return status;
+}
+
+MStatus DollyZoomCmd::undoIt()
+{
+	MStatus status;
+
+	return status;
 }
--- DollyZoomNode/dollyZoomCmd.h
+++ DollyZoomNode/dollyZoomCmd.h
@@ -1,5 +1,9 @@
 #include <maya/MPxCommand.h>
 #include <maya/MSyntax.h>
+#include <maya/MArgDatabase.h>
+#include <maya/MSelectionList.h>
+#include <maya/MFnDependencyNode.h>
+
 #pragma once
 
 class DollyZoomCmd : public MPxCommand
@@ -9,5 +13,9 @@ public:
 	virtual ~DollyZoomCmd();
 	static void* creator();
 	static MStatus doIt(const MArgList &args); 
+	static MStatus redoIt();
+	static MStatus undoIt();
 	static MSyntax newSyntax();
+private:
+	static MSelectionList selectionList;
 };
